cc76932583346ed1d93ec24947bda3a319dbe24a
felipepegoraro <fesousapegora03@gmail.com>
2022-08-10 17:00:39 -0300

    cards...

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Cards from './components/Cards'
 import CardGenerator from './components/CardGenerator'
 import './styles/css/App.css'
 
@@ -8,6 +9,7 @@ function App() {
 				<h1 className="title">Lista de afazeres</h1>
 				<h3 className="subtitle">Coloque aqui suas tarefas do dia</h3>
 				<CardGenerator />
+				<Cards />
 			</div>
     </div>
   );
--- src/components/CardGenerator/index.jsx
+++ src/components/CardGenerator/index.jsx
@@ -1,8 +1,16 @@
+import '../../styles/css/cardGenerator.css'
 
 const CardGenerator = () => {
 	return ( 
-		<div>
-			Card Generator.
+		<div className="card-generator">
+			<form>
+				<label for="task">Coloque sua tarefa aqui.</label>
+				<input type="text" name="task" />
+
+				<div className='card-generator_options'>
+					<button>Criar</button>
+				</div>
+			</form>
 		</div>
 	)
 }
